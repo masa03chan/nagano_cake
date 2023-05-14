6aaed383b999cccf39bf19588d8d0b43157406cc
masa03chan <127210098+masa03chan@users.noreply.github.com>
2023-05-14 09:49:09 +0000

    Edit routes, create header

---
+++ app/controllers/admin/items_controller.rb
@@ -0,0 +1,19 @@
+class Admin::ItemsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,39 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :genres, only: [:index, :create, :edit, :update]
+  end
+  namespace :admin do
+    resources :order_details, only: [:update]
+  end
+  namespace :admin do
+    resources :orders, only: [:show, :update]
+  end
+  namespace :public do
+    resources :customers, only: [:show, :edit, :update]
+  end
+  namespace :admin do
+    resources :customers, only: [:index, :show, :edit, :update]
+  end
+  namespace :admin do
+    resources :items, except: [:destroy]
+  end
+  namespace :public do
+    resources :addresses, except: [:new, :show]
+  end
+  namespace :public do
+    resources :orders, only: [:new, :index, :show]
+  end
+  namespace :public do
+    resources :cart_items, only: [:index, :update, :create, :destroy]
+  end
+  namespace :public do
+    resources :items, only: [:index, :show]
+  end
+  namespace :admin do
+    get 'homes/top' => 'admin/homes#top', as: "admin"
+  end
+  root to: "homes#top"
+  get "home/about" => "homes#about", as: "about"
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
