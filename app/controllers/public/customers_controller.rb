a7b9cf593fa192712243435508941abe1ab0ddf5
masa03chan <127210098+masa03chan@users.noreply.github.com>
2023-05-30 07:55:21 +0000

    Finish delete_process

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -10,9 +10,30 @@ class Public::CustomersController < ApplicationController
 
   def update
     @customer = Customer.find(current_customer.id)
-    @customer.update
-    redirect_to 
+    if @customer.update
+      flash[:notice] = "登録情報を更新しました。"
+      redirect_to customer_path(@customer)
+    else
+      render :edit
+    end
   end
   
+  def delete_confirm
+    
+  end
+  
+  def delete_process
+    @customer = Customer.find_by(email: params[:customer][:email])
+    @customer.update(is_valid: true)
+    reset_session
+    redirect_to public_root_path
+  end
+    
+  
+  private
+
+  def customer_params
+    params.require(:customer).permit(:email, :encrypted_password, :first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :telephone_number)
+  end
   
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,7 @@
 class Customer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  before_action :customer_state, only: [:create]
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
@@ -11,4 +12,15 @@ class Customer < ApplicationRecord
     validates :postal_code, presence: true
     validates :address, presence: true
     validates :telephone_number, presence: true
+    
+  protected
+  
+  def customer_state
+    @customer = Customer.find_by(email: params[:customer][:email])
+    return if !@customer
+    if @customer.valid_password?(params[:customer][:password]) && !@customer.is_deleted
+      flash[:notice] = "退会済みです。再度ご登録をしてご利用ください。"
+      redirect_to new_customer_session_path
+    end
+  end
 end
