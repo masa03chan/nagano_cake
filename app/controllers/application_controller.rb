e20279ab1489f572ebbcf85f5765d6e3150a9c85
masa03chan <127210098+masa03chan@users.noreply.github.com>
2023-06-02 07:48:48 +0000

    edit customer_infomation_edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,17 +6,17 @@ class ApplicationController < ActionController::Base
     when Admin
       admin_root_path
     when Customer
-      public_root_path
+      root_path
     end
   end
 
   def after_sign_out_path_for(resource_or_scope)
     if resource_or_scope == :Customer
-        public_top_path
+      top_path
     elsif resource_or_scope == :admin
-        new_admin_session_path
+      new_admin_session_path
     else
-        public_root_path
+      root_path
     end
   end
 
--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,7 +1,7 @@
 class Public::CustomersController < ApplicationController
   def show
     @customer = Customer.find(current_customer.id)
-    
+
   end
 
   def edit
@@ -10,31 +10,30 @@ class Public::CustomersController < ApplicationController
 
   def update
     @customer = Customer.find(current_customer.id)
-    if @customer.update
-      flash[:notice] = "登録情報を更新しました。"
+    if @customer.update(customer_params)
       redirect_to customer_path(@customer)
     else
       render :edit
     end
   end
-  
+
   def delete_confirm
-    
+
   end
-  
-  def delete_process
+
+  def delete
     @customer = Customer.find_by(email: params[:customer][:email])
     @customer.update(is_valid: true)
     reset_session
     flash[:notice] = "退会処理を実行いたしました。"
     redirect_to public_root_path
   end
-    
-  
+
+
   private
 
   def customer_params
     params.require(:customer).permit(:email, :encrypted_password, :first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :telephone_number)
   end
-  
+
 end
