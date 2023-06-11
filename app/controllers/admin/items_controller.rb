7e3c53966fdba9a120ae9503c10c227b230d0235
masa03chan <127210098+masa03chan@users.noreply.github.com>
2023-06-11 09:44:13 +0000

    Edit item_new

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -7,6 +7,7 @@ class Admin::ItemsController < ApplicationController
 
   def new
     @item = Item.new
+    
   end
 
   def edit
--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,6 @@ class Item < ApplicationRecord
   validates :price, presence: true
   validates :genre_id, presence: true
   
+  
+  
 end
