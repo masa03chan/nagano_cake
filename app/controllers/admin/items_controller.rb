0e3bdbf55a7c4976eb6e2ff2cb36a270f728a5f6
masa03chan <127210098+masa03chan@users.noreply.github.com>
2023-06-19 06:19:08 +0000

    Finish item_edit

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -23,9 +23,17 @@ class Admin::ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      flash[:notice] = "商品情報を編集しました。"
+      redirect_to admin_items_path
+    else
+      render :edit
+    end 
   end
 
   private
