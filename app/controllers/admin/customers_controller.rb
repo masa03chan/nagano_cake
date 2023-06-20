6117791c0361931bf577f55bccbf05d9b328208d
masa03chan <127210098+masa03chan@users.noreply.github.com>
2023-06-20 14:29:10 +0000

    Finish admin_customer_edit

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -24,7 +24,7 @@ class Admin::CustomersController < ApplicationController
   private
   
   def customer_params
-    params.require(:customer).permit()
+    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :telephone_number, :is_deleted)
   end
   
 end
