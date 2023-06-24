891585d804cb983ddb2d6d85a874061eddff4afb
masa03chan <127210098+masa03chan@users.noreply.github.com>
2023-06-24 14:36:55 +0000

    Edit public-item-genre_search

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -2,12 +2,14 @@ class Public::ItemsController < ApplicationController
   def index
     @items = Item.where(is_active: true).page(params[:page]).per(8)
     @genres = Genre.all
-    
+    @quantity = Item.count
   end
 
   def show
     @item = Item.find(params[:id])
     @genres = Genre.all
+    @cart_item = CartItem.new
     
   end
+  
 end
