cd7fa13564abcff6a1e05e10e41e1a7da2d7c5bc
masa03chan <127210098+masa03chan@users.noreply.github.com>
2023-06-26 08:07:55 +0000

    Finish address_index

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -5,22 +5,41 @@ class Public::AddressesController < ApplicationController
   end
 
   def edit
-    
+    @address = Address.find(params[:id])
   end
 
   def create
+    @address = Address.new(address_params)
+    if @address.save
+      flash[:notice] = "配送先を登録しました。"
+      redirect_to addresses_path
+    else
+      @addresses = Address.all
+      render :index
+    end
   end
 
   def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      flash[:notice] = "配送先情報を更新しました。"
+      redirect_to addresses_path
+    else
+      @addresses = Address.all
+      render :index
+    end
   end
 
   def destroy
+    @address = Address.find(params[:id])
+    address.destroy
+    redirect_to addresses_path
   end
-  
+
   private
 
   def address_params
     params.require(:address).permit(:name, :postal_code, :address)
   end
-  
+
 end
