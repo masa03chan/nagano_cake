7b18d38a0fd2f0e7bf702a28e634dd0f8f2f0a60
masa03chan <127210098+masa03chan@users.noreply.github.com>
2023-06-26 05:45:24 +0000

    Finish public_item_genre_search

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,8 +1,11 @@
 class Public::AddressesController < ApplicationController
   def index
+    @address = Address.new
+    @addresses = Address.all
   end
 
   def edit
+    
   end
 
   def create
@@ -13,4 +16,11 @@ class Public::AddressesController < ApplicationController
 
   def destroy
   end
+  
+  private
+
+  def address_params
+    params.require(:address).permit(:name, :postal_code, :address)
+  end
+  
 end
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,8 +1,15 @@
 class Public::ItemsController < ApplicationController
   def index
-    @items = Item.where(is_active: true).page(params[:page]).per(8)
-    @genres = Genre.all
-    @quantity = Item.count
+    if params[:genre_id]
+      @genre = Genre.find(params[:genre_id])
+      @items = @genre.items.where(is_active: true).page(params[:page]).per(8)
+      @genres = Genre.all
+      @quantity = @genre.items.where(is_active: true).count
+    else
+      @items = Item.where(is_active: true).page(params[:page]).per(8)
+      @genres = Genre.all
+      @quantity = Item.count
+    end
   end
 
   def show
