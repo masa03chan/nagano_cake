303556441a9c07e2bbc0b4cc67b168ec019c674c
masa03chan <127210098+masa03chan@users.noreply.github.com>
2023-07-01 15:29:56 +0000

    Finish Cart_item commitment

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,24 +1,46 @@
 class Public::CartItemsController < ApplicationController
+  before_action :authenticate_customer!
+
+
   def index
     @cart_items = CartItem.all
   end
 
   def create
-    @item = Item.find(cart_item_params[:item_id])
     @cart_item = CartItem.new(cart_item_params)
-    if @cart_item.save
+    @cart_item.customer_id = current_customer.id
+    @cart_item.item_id = params[:item_id]
+    if @cart_item.save!
       flash[:notice] = "商品をカートに入れました。"
       redirect_to cart_items_path
     else
+      flash[:alert] = "個数を選択してください"
       @items = Item.all
       render :index
     end
   end
 
   def update
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.customer_id = current_customer.id
+    @cart_item.item_id = params[:item_id]
+    if @cart_item.update(cart_item_params)
+      flash[:notice] = "カート内を更新ました。"
+      redirect_to cart_items_path
+    else
+      flash[:alert] = "個数を選択してください"
+      @items = Item.all
+      render :index
+    end
   end
 
   def destroy
+    @cart_item = CartItem.find(params[:id])
+    CartItem.destroy
+    redirect_to cart_items_path
+  end
+
+  def destroy_all
   end
 
   private
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,8 +1,14 @@
 class CartItem < ApplicationRecord
-  belongs_to :item
+  belongs_to :item, optional: true
   belongs_to :customer
 
   validates :amount, presence: true
+  validates :item_id, presence: true
+  validates :customer_id, presence: true
+
+  def with_tax_price
+    (item.price * 1.1).floor
+  end
 
   def subtotal
     item.with_tax_price * amount
