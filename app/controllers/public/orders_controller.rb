e38a83b9ff293ee0d33c0a6d2aa164b2a36b0135
masa03chan <127210098+masa03chan@users.noreply.github.com>
2023-07-08 15:15:07 +0000

    Edit order_new

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -13,7 +13,23 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
-    
+    @order = Order.new(order_params)
+    if params[:order][:select_address] == "0"
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.first_name + current_customer.last_name
+    elsif params[:order][:select_address] == "1"
+      @address = Address.find(params[:order][:address_id])
+      @order.postal_code = @address.postal_code
+      @order.address = @address.address
+      @order.name = @address.name
+    elsif params[:order][:select_address] == "2"
+      @order = Order.new
+    else  
+      redirect_to new_orders_path
+    end
+    @cart_items = current_customer.cart_items.all
+    @total = @cart_items.inject(0) { |sum, cart_item| sum + cart_item.subtotal }
   end
   
   def complete
