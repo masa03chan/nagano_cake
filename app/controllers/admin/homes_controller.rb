ccef14c1814bae222c0d71c4950de8f32bc74431
masa03chan <127210098+masa03chan@users.noreply.github.com>
2023-07-17 15:10:00 +0000

    Finish admin_order_index

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,4 +1,7 @@
 class Admin::HomesController < ApplicationController
   def top
+    @orders = Order.page(params[:page]).per(10)
+    @order_details = OrderDetail.all
+    @total = @order_details.inject(0){ |sum, order_detail| sum + order_detail.amount }
   end
 end
