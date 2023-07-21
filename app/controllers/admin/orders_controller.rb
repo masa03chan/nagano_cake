c0c973f1e0df56a70e6673bd8886bd80035f34fd
masa03chan <127210098+masa03chan@users.noreply.github.com>
2023-07-21 10:23:54 +0000

    Edit order_detail_update

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,23 @@
 class Admin::OrdersController < ApplicationController
+  before_action :authenticate_admin!
+
   def show
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
   end
 
   def update
+    order = Order.find(params[:id])
+    order_details = order.order_details
+    order.update(order_params)
+    if order.order_status == "payment_confirmation"
+      order_details.update_all(making_status: "waiting_for_making")
+    end
+    redirect_to admins_order_path(order.id)
+  end
+
+private
+  def order_params
+    params.require(:order).permit(:status)
   end
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -2,4 +2,10 @@ class OrderDetail < ApplicationRecord
   belongs_to :item
   belongs_to :order
 
+  enum making_status: { cannot_make: 0, waiting_for_making: 1, in_the_making: 2, completed: 3 }
+
+  validates :price, presence: true
+  validates :amount, presence: true
+  validates :making_status, presence: true
+
 end
